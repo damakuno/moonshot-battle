db4cb1e187fd109e1501a6047814d3918592d0ae
unknown <damakuno@gmail.com>
2020-11-08 21:09:26 +0800

    refactoring main.lua and removing prototype code

--- lib/utils/anime.lua
+++ lib/utils/anime.lua
@@ -1,23 +1,25 @@
 local Anime = {}
 
-function Anime:new(image, width, height, duration, startingSpriteNum, enabled, playTillEnd, object)
-    object =
-        object or
-        {
-            currentTime = 0,
-            spriteSheet = image,
-            width = width,
-            height = height,
-            duration = duration,
-            quads = {},
-            spriteNum = startingSpriteNum or 1,
-            enabled = enabled or false,
-            playTillEnd = playTillEnd or false
-        }
+function Anime:new(image, width, height, duration, startingSpriteNum, enabled,
+                   loop, playTillEnd, object)
+    object = object or {
+        currentTime = 0,
+        spriteSheet = image,
+        width = width,
+        height = height,
+        duration = duration,
+        quads = {},
+        spriteNum = startingSpriteNum or 1,
+        enabled = enabled or false,
+        loop = loop or false,
+        playTillEnd = playTillEnd or false
+    }
 
     for y = 0, image:getHeight() - height, height do
         for x = 0, image:getWidth() - width, width do
-            table.insert(object.quads, love.graphics.newQuad(x, y, width, height, image:getDimensions()))
+            table.insert(object.quads, love.graphics.newQuad(x, y, width,
+                                                             height,
+                                                             image:getDimensions()))
         end
     end
 
@@ -27,17 +29,19 @@ function Anime:new(image, width, height, duration, startingSpriteNum, enabled, p
 end
 
 function Anime:update(dt)
-    if self.enabled == true then
-        self.currentTime = self.currentTime + dt
-        if self.currentTime >= self.duration then
-            self.currentTime = self.currentTime - self.duration
-        end
-    else
-        if self.playTillEnd == true then
-            if not (self.spriteNum == 1) then
-                self.currentTime = self.currentTime + dt
-                if self.currentTime >= self.duration then
-                    self.currentTime = self.currentTime - self.duration
+    if (self.spriteNum < #self.quads) or self.loop then
+        if self.enabled == true then
+            self.currentTime = self.currentTime + dt
+            if self.currentTime >= self.duration then
+                self.currentTime = self.currentTime - self.duration
+            end
+        else
+            if self.playTillEnd == true then
+                if not (self.spriteNum == 1) then
+                    self.currentTime = self.currentTime + dt
+                    if self.currentTime >= self.duration then
+                        self.currentTime = self.currentTime - self.duration
+                    end
                 end
             end
         end
@@ -45,16 +49,14 @@ function Anime:update(dt)
 end
 
 function Anime:draw(x, y, r, sx, sy, ox, oy, kx, ky)
-    self.spriteNum = math.floor(self.currentTime / self.duration * #self.quads) + 1
-    love.graphics.draw(self.spriteSheet, self.quads[self.spriteNum], x, y, r, sx, sy, ox, oy, kx, ky)
+    self.spriteNum =
+        math.floor(self.currentTime / self.duration * #self.quads) + 1
+    love.graphics.draw(self.spriteSheet, self.quads[self.spriteNum], x, y, r,
+                       sx, sy, ox, oy, kx, ky)
 end
 
-function Anime:start()
-    self.enabled = true
-end
+function Anime:start() self.enabled = true end
 
-function Anime:stop()
-    self.enabled = false
-end
+function Anime:stop() self.enabled = false end
 
 return Anime
--- main.lua
+++ main.lua
@@ -5,43 +5,30 @@ Dialog = require "lib.utils.dialog"
 
 function love.load()
     deltaTime = 0
+
     font = love.graphics.newFont("res/fonts/lucon.ttf", 12)
     dialog_font = love.graphics.newFont("res/fonts/lucon.ttf", 20)
+    
+    dialog_text = "this is some long text " .. "this is some long text " ..
+                      "this is some long text " .. "this is some long text " ..
+                      "this is some long text " .. "this is some long text " ..
+                      "this is some long text " .. "this is some long text " ..
+                      "this is some long text " .. "this is some long text"
+    dialog_text2 = "some other long text " .. "some other long text " ..
+                       "some other long text " .. "some other long text " ..
+                       "some other long text " .. "some other long text " ..
+                       "some other long text " .. "some other long text " ..
+                       "some other long text " .. "some other long text"
+    mikiAnime = Anime:new(love.graphics
+                              .newImage("res/images/KiraMikiSprite.png"), 205,
+                          200, 1, 1)
+    jillAnime = Anime:new(love.graphics.newImage("res/images/Jill.png"), 205,
+                          190, 1, 1)
 
-    str_index = 1
-    dialog_text =
-        "this is some long text " ..
-        "this is some long text " ..
-            "this is some long text " ..
-                "this is some long text " ..
-                    "this is some long text " ..
-                        "this is some long text " ..
-                            "this is some long text " ..
-                                "this is some long text " .. "this is some long text " .. "this is some long text"
-    dialog_text2 =
-        "some other long text " ..
-        "some other long text " ..
-            "some other long text " ..
-                "some other long text " ..
-                    "some other long text " ..
-                        "some other long text " ..
-                            "some other long text " ..
-                                "some other long text " .. "some other long text " .. "some other long text"
-    ohAnime = Anime:new(love.graphics.newImage("res/images/oldHero.png"), 16, 18, 1, 1, false, true)
-    mikiAnime = Anime:new(love.graphics.newImage("res/images/KiraMikiSprite.png"), 205, 200, 1, 1, false)
-    jillAnime = Anime:new(love.graphics.newImage("res/images/Jill.png"), 205, 190, 1, 1, false)
-
-    dialog = Dialog:new(mikiAnime, dialog_text, dialog_font, 10, 500, 760, "left", 0.04)
+    dialog = Dialog:new(ohAnime, dialog_text, dialog_font, 10, 500, 760, "left",
+                        0.04)
     dialog:start()
 
-    counter = 0
-    -- This should go into game state
-    velocity = {x = 0, y = 0, px = 0, py = 0}
-    acceleration = {x = 0, y = 0}
-    imgMoon = love.graphics.newImage("res/images/moon.png")
-    imgPos = {facing_x = 1, x = 20, y = 20, px = 20, py = 20}
-
-    timer = Timer:new(0.2, calcVelocity)
     keybind = Keybind:new()
     -- Example setting of key bindings
     -- keybind:set("UP", "w")
@@ -49,66 +36,24 @@ function love.load()
 end
 
 function love.draw()
-    -- love.graphics.draw(imgMoon, imgPos.x, imgPos.y)
-    ohAnime:draw(imgPos.x, imgPos.y, 0, imgPos.facing_x * 4, 4, ohAnime.width / 2, 0, 0, 0)
     show_vars()
     draws(dialog)
 end
 
 function love.update(dt)
     deltaTime = dt
-    updates(dt, ohAnime, timer, dialog)
-
-    if love.keyboard.isDown(keybind.UP) then
-        imgPos.y = imgPos.y - 1
-        ohAnime:start()
-    end
-    if love.keyboard.isDown(keybind.DOWN) then
-        imgPos.y = imgPos.y + 1
-        ohAnime:start()
-    end
-    if love.keyboard.isDown(keybind.LEFT) then
-        imgPos.x = imgPos.x - 1
-        imgPos.facing_x = -1
-        ohAnime:start()
-    end
-    if love.keyboard.isDown(keybind.RIGHT) then
-        imgPos.x = imgPos.x + 1
-        imgPos.facing_x = 1
-        ohAnime:start()
-    end
+    updates(dt, timer, dialog)
 end
 
 function love.keyreleased(key)
-    if key == keybind.UP then
-        ohAnime:stop()
-    end
-    if key == keybind.DOWN then
-        ohAnime:stop()
-    end
-    if key == keybind.LEFT then
-        ohAnime:stop()
-    end
-    if key == keybind.RIGHT then
-        ohAnime:stop()
-    end
-
     if key == keybind.SPACE then
         dialog:skipDialog()
         dialog:setNewDialog(dialog_text2, jillAnime)
     end
 end
 
-function show_vars()    
+function show_vars()
     love.graphics.print("dt: " .. deltaTime, font, 10, 10)
-    love.graphics.print("x: " .. imgPos.x, font, 10, 20)
-    love.graphics.print("y: " .. imgPos.y, font, 10, 30)
-    love.graphics.print("px: " .. imgPos.px, font, 10, 40)
-    love.graphics.print("py: " .. imgPos.py, font, 10, 50)
-    love.graphics.print("x velocity: " .. velocity.x, font, 10, 60)
-    love.graphics.print("y velocity: " .. velocity.y, font, 10, 70)
-    love.graphics.print("x acceleration: " .. acceleration.x, font, 10, 80)
-    love.graphics.print("y acceleration: " .. acceleration.y, font, 10, 90)
 end
 
 function calcVelocity(ticks, counter)
@@ -124,23 +69,10 @@ end
 
 function updates(dt, ...)
     local args = {...}
-    for i, arg in ipairs(args) do
-        arg:update(dt)
-    end
+    for i, arg in ipairs(args) do arg:update(dt) end
 end
 
 function draws(...)
     local args = {...}
-    for i, arg in ipairs(args) do
-        arg:draw()
-    end
+    for i, arg in ipairs(args) do arg:draw() end
 end
-
--- example starting and stopping animation based on timer
--- function controlAnim(ticks, counter)
---     if ohAnime.enabled then
---         ohAnime:stop()
---     else
---         ohAnime:start()
---     end
--- end
