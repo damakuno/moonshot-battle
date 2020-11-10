58469d2f5d94ea5a8f868effba9f2544e9a1c693
damakuno <damakuno@gmail.com>
2020-11-10 14:48:30 +0800

    modified anime to default to image width and height if not specified

---
+++ experiments/m3game/lib/utils/anime.lua
@@ -0,0 +1,83 @@
+local Anime = {}
+
+function Anime:new(
+    name,
+    image,
+    width,
+    height,
+    duration,
+    startingSpriteNum,
+    enabled,
+    loop,
+    playTillEnd,
+    dialogPosition,
+    object)
+    if width == nil then
+        width = image:getWidth()
+    end
+    if height == nil then
+        height = image:getHeight()
+    end
+
+    object =
+        object or
+        {
+            currentTime = 0,
+            name = name,
+            spriteSheet = image,
+            width = width,
+            height = height,
+            duration = duration or 1,
+            quads = {},
+            spriteNum = startingSpriteNum or 1,
+            enabled = enabled or false,
+            loop = loop or false,
+            playTillEnd = playTillEnd or false,
+            dialogPosition = dialogPosition or "left"
+        }
+
+    for y = 0, image:getHeight() - height, height do
+        for x = 0, image:getWidth() - width, width do
+            table.insert(object.quads, love.graphics.newQuad(x, y, width, height, image:getDimensions()))
+        end
+    end
+
+    setmetatable(object, self)
+    self.__index = self
+    return object
+end
+
+function Anime:update(dt)
+    if (self.spriteNum < #self.quads) or self.loop then
+        if self.enabled == true then
+            self.currentTime = self.currentTime + dt
+            if self.currentTime >= self.duration then
+                self.currentTime = self.currentTime - self.duration
+            end
+        else
+            if self.playTillEnd == true then
+                if not (self.spriteNum == 1) then
+                    self.currentTime = self.currentTime + dt
+                    if self.currentTime >= self.duration then
+                        self.currentTime = self.currentTime - self.duration
+                    end
+                end
+            end
+        end
+    end
+end
+
+function Anime:draw(x, y, r, sx, sy, ox, oy, kx, ky)
+    self.spriteNum = math.floor(self.currentTime / self.duration * #self.quads) + 1
+    love.graphics.draw(self.spriteSheet, self.quads[self.spriteNum], x, y, r, sx, sy, ox, oy, kx, ky)
+end
+
+function Anime:start()
+    self.enabled = true
+end
+
+function Anime:stop()
+    self.enabled = false
+end
+
+return Anime
--- experiments/m3game/main.lua
+++ experiments/m3game/main.lua
@@ -1,20 +1,28 @@
 local Keybind = require "lib.utils.keybind"
+local Anime = require "lib.utils.anime"
 
 function love.load()
     deltaTime = 0
     font = love.graphics.newFont("res/fonts/lucon.ttf", 12)
     moons = {
-        damage = love.graphics.newImage("res/images/moonDamage.png"),
-        freeze = love.graphics.newImage("res/images/moonFreeze.png"),
-        heal = love.graphics.newImage("res/images/moonHeal.png"),
-        meter = love.graphics.newImage("res/images/moonMeter.png"),
-        shield = love.graphics.newImage("res/images/moonShield.png")
+        damage = Anime:new("damage", love.graphics.newImage("res/images/moonDamage.png")),
+        freeze = Anime:new("freeze", love.graphics.newImage("res/images/moonFreeze.png")),
+        heal = Anime:new("heal", love.graphics.newImage("res/images/moonHeal.png")),
+        meter = Anime:new("meter", love.graphics.newImage("res/images/moonMeter.png")),
+        shield = Anime:new("shield", love.graphics.newImage("res/images/moonShield.png"))
     }
 
     keybind = Keybind:new()
 end
 
 function love.draw()
+    local screenWidth = love.graphics.getWidth()
+    local screenHeight = love.graphics.getHeight()
+    -- love.graphics.print("Screen width: "..screenWidth, font, 10, 10)
+    -- love.graphics.print("Screen height: "..screenHeight, font, 10, 20)
+    love.graphics.rectangle("line", 10, 10, 780 / 2, 580)
+    love.graphics.rectangle("line", screenWidth / 2, 10, 780 / 2, 580)
+    moons.damage:draw(20, 20, 0, 0.5, 0.5)
 end
 
 function love.update(dt)
@@ -22,7 +30,6 @@ function love.update(dt)
 end
 
 function love.keyreleased(key)
-
 end
 
 function updates(dt, ...)
