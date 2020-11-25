0f5db9f6e1e945f3d97e1d5a751babe010b9c6af
unknown <damakuno@gmail.com>
2020-11-25 20:09:02 +0800

    rebalanced meter and added draw for meter

--- experiments/m3game/lib/core/chara.lua
+++ experiments/m3game/lib/core/chara.lua
@@ -3,24 +3,22 @@ local LIP = require "lib.utils.LIP"
 local Timer = require "lib.utils.timer"
 
 function Chara:new(charaFile, object)
-    object =
-        object or
-        {
-            charaFile = charaFile,
-            state = {},
-            grid = {},
-            enemy = {},
-            updates = {},
-            shieldCurrentTime = 0,
-            shieldDuration = 0,
-            shielded = false,
-            specialCurrentTime = 0,
-            specialDuration = 0,
-            specialActive = false,
-            callback = {},
-            callbackFlag = {},
-            dead = false
-        }
+    object = object or {
+        charaFile = charaFile,
+        state = {},
+        grid = {},
+        enemy = {},
+        updates = {},
+        shieldCurrentTime = 0,
+        shieldDuration = 0,
+        shielded = false,
+        specialCurrentTime = 0,
+        specialDuration = 0,
+        specialActive = false,
+        callback = {},
+        callbackFlag = {},
+        dead = false
+    }
     object.state = LIP.load(object.charaFile)
     -- print("\n")
     -- for k, v in pairs(object.state) do
@@ -53,7 +51,7 @@ function Chara:update(dt)
         if self.specialCurrentTime >= self.specialDuration then
             self.specialCurrentTime = 0
             -- self.specialDuration = 0
-            self.state.stats.meter = self.state.stats.meter - math.ceil(self.state.stats.maxmeter / 5)
+            self.state.stats.meter = self.state.stats.meter - 3 
             if self.state.stats.meter < 1 then
                 self.specialActive = false
                 self.state.stats.meter = 0
@@ -73,18 +71,17 @@ function Chara:draw(x, y, align)
         love.graphics.rectangle("fill", x, y, self.state.stats.hp * 3, 20)
         love.graphics.setColor(0 / 255, 0 / 255, 0 / 255, 1)
         love.graphics.print(self.state.stats.hp, font, x + 4, y + 4)
+        love.graphics.setColor(255 / 255, 145 / 255, 0 / 255, 1)
+        love.graphics.rectangle("fill", x, y + 20, self.state.stats.maxmeter * 3, 20)
+        love.graphics.setColor(225 / 255, 255 / 255, 105 / 255, 1)
+        love.graphics.rectangle("fill", x, y + 20, self.state.stats.meter * 3, 20)
     end
     if align == "right" then
         love.graphics.setColor(255 / 255, 0 / 255, 0 / 255, 1)
         love.graphics.rectangle("fill", x, y, self.state.stats.maxhp * 3, 20)
         love.graphics.setColor(0 / 255, 255 / 255, 0 / 255, 1)
-        love.graphics.rectangle(
-            "fill",
-            ((self.state.stats.maxhp - self.state.stats.hp) * 3) + x,
-            y,
-            self.state.stats.hp * 3,
-            20
-        )
+        love.graphics.rectangle("fill", ((self.state.stats.maxhp - self.state.stats.hp) * 3) + x, y,
+            self.state.stats.hp * 3, 20)
         love.graphics.setColor(0 / 255, 0 / 255, 0 / 255, 1)
         love.graphics.print(self.state.stats.hp, font, x + 4, y + 4)
     end
@@ -150,7 +147,7 @@ function Chara:specialActivate()
     if self.state.stats.meter >= self.state.stats.maxmeter then
         -- self.state.stats.meter = 0
         self.specialActive = true
-        self.specialDuration = 1 --self.specialDuration + self.state.stats.special
+        self.specialDuration = 1 -- self.specialDuration + self.state.stats.special
     end
 end
 
@@ -163,78 +160,75 @@ function Chara:initCallbacks()
         self.callbackFlag["dead"] = false
     end
 
-    self.grid:registerCallback(
-        "clearedMatches",
-        function(g, res)
-            -- print("Matched:")
-            for k, v in ipairs(res) do
-                -- print("[" .. moons[k].name .. "]: " .. v)
-                if v > 0 then
-                    local specialMultiplier = 1
-                    if self.specialActive then
-                        specialMultiplier = 2
-                    else
-                        specialMultiplier = 1
-                    end
-                    local f = function()
-                    end
-                    local timer = Timer:new(1, f, true)
-                    -- Damage
-                    if k == 1 then
-                        f = function(t)
-                            self.enemy:takeDamage(self.state.stats.damage * v * specialMultiplier)
-                            t.enabled = false
-                        end
-                        timer = Timer:new(1, f, true)
-                    end
-                    -- Freeze
-                    if k == 2 then
-                        -- TODO freeze function
-                        local tilesToFreeze = self.enemy.grid:getUnfrozenTiles(v * specialMultiplier)
-                        for k, tile in ipairs(tilesToFreeze) do
-                            -- print("freeze " .. self.enemy.charaFile .. " for x: " .. tile.x .. " y: " .. tile.y)
-                            self.enemy.grid:freezeTile(tile.x, tile.y)
-                        end
-                        f = function(t)
-                            -- TODO end freeze function
-                            -- set this to freeze duration
-                            if t.accumulator == self.state.stats.freeze then
-                                for k, tile in ipairs(tilesToFreeze) do
-                                    self.enemy.grid:unfreezeTile(tile.x, tile.y)
-                                end
-                                t.enabled = false
-                            end
-                        end
-                        timer = Timer:new(1, f, true)
-                    end
-                    -- Heal
-                    if k == 3 then
-                        f = function(t)
-                            self:heal(self.state.stats.heal * v * specialMultiplier)
-                            t.enabled = false
-                        end
-                        timer = Timer:new(0.2, f, true)
-                    end
-                    -- Meter
-                    if k == 4 then
-                        f = function(t)
-                            self:fillMeter(v)
-                            t.enabled = false
-                        end
-                        timer = Timer:new(0.2, f, true)
-                    end
-                    -- shield
-                    if k == 5 then
-                        self.shieldDuration = self.shieldDuration + (self.state.stats.shield * v * specialMultiplier)
-                        if self.shielded == false then
-                            self.shielded = true
-                        end
-                    end
-                    table.insert(self.updates, timer)
+    self.grid:registerCallback("clearedMatches", function(g, res)
+        -- print("Matched:")
+        for k, v in ipairs(res) do
+            -- print("[" .. moons[k].name .. "]: " .. v)
+            if v > 0 then
+                local specialMultiplier = 1
+                if self.specialActive then
+                    specialMultiplier = 2
+                else
+                    specialMultiplier = 1
                 end
+                local f = function()
+                end
+                local timer = Timer:new(1, f, true)
+                -- Damage
+                if k == 1 then
+                    f = function(t)
+                        self.enemy:takeDamage(self.state.stats.damage * v * specialMultiplier)
+                        t.enabled = false
+                    end
+                    timer = Timer:new(1, f, true)
+                end
+                -- Freeze
+                if k == 2 then
+                    -- TODO freeze function
+                    local tilesToFreeze = self.enemy.grid:getUnfrozenTiles(v * specialMultiplier)
+                    for k, tile in ipairs(tilesToFreeze) do
+                        -- print("freeze " .. self.enemy.charaFile .. " for x: " .. tile.x .. " y: " .. tile.y)
+                        self.enemy.grid:freezeTile(tile.x, tile.y)
+                    end
+                    f = function(t)
+                        -- TODO end freeze function
+                        -- set this to freeze duration
+                        if t.accumulator == self.state.stats.freeze then
+                            for k, tile in ipairs(tilesToFreeze) do
+                                self.enemy.grid:unfreezeTile(tile.x, tile.y)
+                            end
+                            t.enabled = false
+                        end
+                    end
+                    timer = Timer:new(1, f, true)
+                end
+                -- Heal
+                if k == 3 then
+                    f = function(t)
+                        self:heal(self.state.stats.heal * v * specialMultiplier)
+                        t.enabled = false
+                    end
+                    timer = Timer:new(0.2, f, true)
+                end
+                -- Meter
+                if k == 4 then
+                    f = function(t)
+                        self:fillMeter(v)
+                        t.enabled = false
+                    end
+                    timer = Timer:new(0.2, f, true)
+                end
+                -- shield
+                if k == 5 then
+                    self.shieldDuration = self.shieldDuration + (self.state.stats.shield * v * specialMultiplier)
+                    if self.shielded == false then
+                        self.shielded = true
+                    end
+                end
+                table.insert(self.updates, timer)
             end
         end
-    )
+    end)
 end
 
 function Chara:getSpawnTable()
